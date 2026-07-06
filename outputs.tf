e1730d612fde4eca54ecb4869204225ceb88a1ce
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "policy_virtual_machine_configuration_assignments" {
   description = "All policy_virtual_machine_configuration_assignment resources"
   value       = azurerm_policy_virtual_machine_configuration_assignment.policy_virtual_machine_configuration_assignments
 }
+output "policy_virtual_machine_configuration_assignments_configuration" {
+  description = "List of configuration values across all policy_virtual_machine_configuration_assignments"
+  value       = [for k, v in azurerm_policy_virtual_machine_configuration_assignment.policy_virtual_machine_configuration_assignments : v.configuration]
+}
+output "policy_virtual_machine_configuration_assignments_location" {
+  description = "List of location values across all policy_virtual_machine_configuration_assignments"
+  value       = [for k, v in azurerm_policy_virtual_machine_configuration_assignment.policy_virtual_machine_configuration_assignments : v.location]
+}
+output "policy_virtual_machine_configuration_assignments_name" {
+  description = "List of name values across all policy_virtual_machine_configuration_assignments"
+  value       = [for k, v in azurerm_policy_virtual_machine_configuration_assignment.policy_virtual_machine_configuration_assignments : v.name]
+}
+output "policy_virtual_machine_configuration_assignments_virtual_machine_id" {
+  description = "List of virtual_machine_id values across all policy_virtual_machine_configuration_assignments"
+  value       = [for k, v in azurerm_policy_virtual_machine_configuration_assignment.policy_virtual_machine_configuration_assignments : v.virtual_machine_id]
+}
 
